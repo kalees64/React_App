1db6d87376fe2df0af7d2d600d4840008550e064
kalees64 <vkalees64@gmail.com>
2024-02-10 07:13:13 -0800

    Header Update 2

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,7 @@ import React from 'react'
 function Header(){
     return(
         <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
+            <h1>I am Kalees</h1>
         </header>
     )
 }
