06fc51fca9736f4906201ef24f7aed8be38a95da
kalees64 <vkalees64@gmail.com>
2024-02-13 14:28:50 -0800

    app Launch steps

--- src/About.js
+++ src/About.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './App.css'
 
 const About = () => {
   return (
-    <div>
-        <h1>I am Kalees</h1>
+    <div className='about'>
+        <h1>VK64</h1>
+        <p>This app was created by the reason of practicing react in a beginner level.</p>
+        <a href='https://vk64.netlify.app'>Visit my Site Here</a>
     </div>
   )
 }
